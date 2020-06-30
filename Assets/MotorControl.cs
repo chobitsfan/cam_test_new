9b5b61ff9c95203cdb075867ae3e7a5f1f834902
chobitsfan <chobits@itri.org.tw>
2020-06-30 17:13:23 +0800

    integrate motor control & demo

---
+++ Assets/MotorControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class MotorControl : MonoBehaviour
+{
+    IPEndPoint motorCtrl = new IPEndPoint(IPAddress.Parse("192.168.50.21"), 6666);
+    Socket ctrlSock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+        byte[] buf = {122, 110, 90};
+        ctrlSock.SendTo(buf, motorCtrl);
+    }
+}
